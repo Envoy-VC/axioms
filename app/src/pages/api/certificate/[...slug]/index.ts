97bbf6c77418ce9e3499f323bf63cdf9db419fdb
Vedant Chainani <vedantchainani1084@gmail.com>
2023-10-21 20:12:28 +0530

    add api logic for metadata retrival

---
+++ app/src/pages/api/certificate/[...slug]/index.ts
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const { slug } = req.query;
+	if (slug?.length !== 2)
+		return res
+			.status(400)
+			.json({ message: 'Manifest Id and Address is Required' });
+
+	const [manifestId, address] = slug;
+
+	const { eventName, eventDescription, eventType, holders, type } = await fetch(
+		`https://gateway.irys.xyz/${manifestId}/metadata.json`
+	)
+		.then((res) => res.json() as any)
+		.catch((err) => console.log(err));
+
+	const imageFile = await fetch(`https://gateway.irys.xyz/${manifestId}`)
+		.then((res) => res.json() as any)
+		.then((res) =>
+			Object.keys(res.paths as string[])
+				.filter((path) => !path.endsWith('.json'))
+				.at(0)
+		)
+		.catch((err) => console.log(err));
+
+	const holder = (holders as any[]).find((holder) => holder.address === address);
+	delete holder.address;
+
+	const openSeaAttributes = Object.entries(holder).map(([key, value], index) => {
+		return {
+			trait_type: key,
+			value,
+		};
+	});
+
+	const metadata = {
+		name: eventName,
+		description: eventDescription,
+		image: `https://gateway.irys.xyz/${manifestId}/${imageFile ?? ''}`,
+		attributes: openSeaAttributes,
+	};
+
+	res.status(200).json(metadata);
+}
---
+++ app/src/pages/certificate/[address]/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import type { ReactElement } from 'react';
+
+import { Button } from 'antd';
+
+import { Layout } from '~/components';
+
+import type { NextPageWithLayout } from '../../_app';
+
+const CertificatePage: NextPageWithLayout = () => {
+	const onFetch = async () => {
+		const res = await fetch(
+			'/api/certificate/DIKO1fJ5yLSSH0DdzLO3VBiZhMZ2KiOZuo88nYeJ11A/0xBF4979305B43B0eB5Bb6a5C67ffB89408803d3e1',
+			{
+				method: 'GET',
+			}
+		);
+		const data = (await res.json()) as object;
+		console.log(data);
+	};
+	return (
+		<div>
+			<Button
+				// eslint-disable-next-line @typescript-eslint/no-misused-promises
+				onClick={onFetch}
+			>
+				Fetch
+			</Button>
+		</div>
+	);
+};
+
+CertificatePage.getLayout = function getLayout(page: ReactElement) {
+	return <Layout>{page}</Layout>;
+};
+
+export default CertificatePage;
