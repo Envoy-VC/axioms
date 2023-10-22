6bd6a154f3d5af6e8b98a70a602fe3dccb5c3590
Vedant Chainani <vedantchainani1084@gmail.com>
2023-10-22 14:59:21 +0530

    add home page

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,16 +1,47 @@
 import type { ReactElement } from 'react';
 
+import { useRouter } from 'next/router';
+
 import { Button } from 'antd';
 
 import { Layout } from '~/components';
-import { useFundIrys } from '~/hooks';
 
 import type { NextPageWithLayout } from './_app';
 
 const Home: NextPageWithLayout = () => {
-	const { fundNode } = useFundIrys();
-	// eslint-disable-next-line @typescript-eslint/no-misused-promises
-	return <Button onClick={fundNode}>Fund Node</Button>;
+	const router = useRouter();
+	return (
+		<div className='w-full p-6 sm:p-16'>
+			<div className='mx-auto flex max-w-4xl flex-col gap-16 p-3 text-center'>
+				<div className='text-5xl font-semibold sm:text-7xl'>
+					<span className='text-secondary'>Verifiable Certifications</span> for
+					everyone.
+				</div>
+				<div className='mx-auto max-w-4xl text-lg'>
+					Axioms provides a one stop solution for organizations to issue and verify
+					certificates. Organizations are{' '}
+					<span className='font-medium text-primary'>Safe Multi-Sig Wallets</span>{' '}
+					and certificates are permanently stored on{' '}
+					<span className='font-medium text-primary'>Arweave</span> and user
+					verification takes place with zkProofs using{' '}
+					<span className='font-medium text-primary'>Sismo Connect</span>.
+				</div>
+				<Button
+					className='mx-auto w-full max-w-[12rem] bg-secondary'
+					size='large'
+					type='primary'
+					// eslint-disable-next-line @typescript-eslint/no-misused-promises
+					onClick={() =>
+						router
+							.push('/login?type=organization')
+							.catch((error) => console.log(error))
+					}
+				>
+					Get Started
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 Home.getLayout = function getLayout(page: ReactElement) {
